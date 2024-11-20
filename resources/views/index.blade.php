400b24c11f792ecb0b1d137759dddffd4576c6ea
Tom Williams <tom.williams0593@outlook.com>
2024-11-20 20:04:42 +0000

    Learnt a bit of routing, blade templates & blade directives.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+<div>
+    Hello, I'm a blade tempate!
+</div>
+
+@isset($name)
+    The name is: {{ $name }}
+@endisset
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,28 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index', [
+        'name'=> 'Tom'
+    ]);
 });
+
+Route::get('/hello', function () {
+    return 'Hello';
+})->name('hello');
+
+Route::get('/hallo', function () {
+    return redirect('/hello')->route('hello');
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return 'Hello ' . $name . '!';
+});
+
+Route::fallback(function () {
+    return 'Still got somewhere!';
+});
+
+// GET - Read data, fetch documents/sites.
+// POST - Store new data (e.g. forms).
+// PUT - To modify existing data.
+// DELETE - Delete data.
