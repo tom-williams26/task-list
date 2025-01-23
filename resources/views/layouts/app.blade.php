ea8e351232fa0cab2a92a3a28d2e0519890b1020
Tom Williams <tom.williams0593@outlook.com>
2025-01-23 03:05:17 +0000

    Creating sessions, error & flash messages.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,15 @@
 
 <head>
     <title>Task List App</title>
+    @yield('styles')
 </head>
 
 <body>
     <h1>@yield('title')</h1>
     <div>
+        @if (session()->has('success'))
+            <div>{{ session('success') }}</div>
+        @endif
         @yield('content')
     </div>
 </body>
