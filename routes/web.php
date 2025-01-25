3be0546f238384ba60bbbc85d034ff51f4cfa9b9
Tom Williams <tom.williams0593@outlook.com>
2025-01-25 20:48:31 +0000

    Created form task request to encapsulate validation logic and authorization rules.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Models\Task;
-use Illuminate\Http\Response;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Requests\TaskRequest;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -29,54 +30,25 @@ Route::get('/tasks/{task}', function (Task $task) {
     ]);
 })->name('tasks.show'); // show is used for returning a single task.
 
-Route::post('/tasks', function (Request $request) {
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required'
-    ]);
+Route::post('/tasks', function (TaskRequest $request) {
 
-    $task = new Task;
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->save();
+    $task = Task::create($request->validated());
 
     return redirect()->route('tasks.show', [
-        'id' => $task->id
+        'task' => $task->id
         ])
         ->with('success', 'Task created successfully!');
 })->name('tasks.store');
 
-Route::put('/tasks/{task}', function (Task $task, Request $request) {
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required'
-    ]);
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
 
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->save();
+    $task->update($request->validated());
 
     return redirect()->route('tasks.show', [
-        'id' => $task->id
+        'task' => $task->id
         ])
         ->with('success', 'Task updated successfully!');
 })->name('tasks.update');
-
-// Route::get('/hello', function () {
-//     return 'Hello';
-// })->name('hello');
-
-// Route::get('/hallo', function () {
-//     return redirect('/hello')->route('hello');
-// });
-
-// Route::get('/greet/{name}', function ($name) {
-//     return 'Hello ' . $name . '!';
-// });
 
 Route::fallback(function () {
     return 'Still got somewhere!';
